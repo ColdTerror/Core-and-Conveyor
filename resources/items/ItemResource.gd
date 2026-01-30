e36068a87ca05c41b8d49a6f6180382df85d0423
Thomas Martin <116772338+ColdTerror@users.noreply.github.com>
2026-01-30 09:39:12 -0500

    Moved item resources into more specific folders and added combat stats to itemresource

--- resources/items/ItemResource.gd
+++ resources/items/ItemResource.gd
@@ -2,7 +2,13 @@
 extends Resource
 class_name ItemResource
 
-@export var display_name: String = "Unknown"   # Name of the item
-@export var max_stack: int = 50               # How many fit in one stack
+@export var display_name: String = "Unknown"   # Name of the item              
 @export var texture: Texture2D                 # Sprite to display on belt / in inventory
 @export var description: String = ""           # Optional: flavor text
+
+
+@export_group("Combat Stats")
+@export var is_ammo: bool = false
+@export var damage: int = 0
+@export var stack_size: int = 1 # How much ammo per item
+@export var projectile_speed: float = 400.0 # How fast it flies
