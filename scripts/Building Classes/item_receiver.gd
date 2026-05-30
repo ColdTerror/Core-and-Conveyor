80e50291afb53cd28507ad6753ba67b037e2180b
Thomas Martin <116772338+ColdTerror@users.noreply.github.com>
2026-05-29 21:04:42 -0400

    item receiver now only pushes items to adjencent belts facing away from building

--- scripts/Building Classes/item_receiver.gd
+++ scripts/Building Classes/item_receiver.gd
@@ -97,56 +97,71 @@ func _unload_items_to_belts():
 	var bm = level_ref.building_manager
 	var footprint = get_footprint(grid_origin)
 	
-	var adjacent_tiles = []
+	var push_directions = [Vector2i.UP, Vector2i.DOWN, Vector2i.LEFT, Vector2i.RIGHT]
+	
 	for tile in footprint:
-		var neighbors = [
-			tile + Vector2i.UP,
-			tile + Vector2i.DOWN,
-			tile + Vector2i.LEFT,
-			tile + Vector2i.RIGHT
-		]
-		for n in neighbors:
-			if not footprint.has(n) and not adjacent_tiles.has(n):
-				adjacent_tiles.append(n)
+		for offset in push_directions:
+			if items_buffered.is_empty():
+				return
 				
-	for adj_pos in adjacent_tiles:
-		if items_buffered.is_empty():
-			break
-			
-		if bm.occupied_tiles.has(adj_pos):
-			var b = bm.occupied_tiles[adj_pos]
-			if b:
-				var next_item = items_buffered[0]
-				if b.has_method("add_item") and b.has_method("can_accept_item"):
-					if b.can_accept_item(next_item):
-						var accepted = b.add_item(next_item, 1)
-						if accepted > 0:
-							items_buffered.remove_at(0)
-							inventory_changed.emit()
-							
-							if is_selected:
-								var hud = get_tree().get_first_node_in_group("MainHUD")
-								if hud and hud.has_node("Popup_Layer/DetailMenu"):
-									hud.get_node("Popup_Layer/DetailMenu").refresh_ui()
+			var adj_pos = tile + offset
+			if footprint.has(adj_pos):
+				continue
+				
+			if bm.occupied_tiles.has(adj_pos):
+				var b = bm.occupied_tiles[adj_pos]
+				if b:
+					var next_item = items_buffered[0]
+					
+					# 1. Adjacent standard buildings (using add_item/can_accept_item)
+					if b.has_method("add_item") and b.has_method("can_accept_item"):
+						if b.can_accept_item(next_item):
+							var accepted = b.add_item(next_item, 1)
+							if accepted > 0:
+								items_buffered.remove_at(0)
+								inventory_changed.emit()
+								
+								if is_selected:
+									var hud = get_tree().get_first_node_in_group("MainHUD")
+									if hud and hud.has_node("Popup_Layer/DetailMenu"):
+										hud.get_node("Popup_Layer/DetailMenu").refresh_ui()
+										
+					# 2. Adjacent logistics belts/routers/filters/bridges (using accept_item_node)
+					elif b.has_method("accept_item_node") and b.has_method("accepts_item_at"):
+						if b.accepts_item_at(adj_pos):
+							var can_output = false
+							if b is RouterBuilding:
+								can_output = true
+							elif b is ConveyorBuilding or b is FilterBuilding:
+								if b.direction == offset:
+									can_output = true
+							else:
+								can_output = true
+								
+							if can_output:
+								var item_scene = load("res://scenes/buildings & related/belts & items/item.tscn")
+								var new_item = item_scene.instantiate()
+								if new_item.has_method("setup"): new_item.setup(level_ref)
+								new_item.item_data = next_item
+								
+								# Snapping position to the edge of the footprint tile in the direction of the offset
+								var tile_center_px = level_ref.object_layer.map_to_local(tile)
+								var edge_px = tile_center_px + (Vector2(offset) * 16.0)
+								new_item.global_position = edge_px
+								
+								if new_item.has_method("_ready"): new_item._ready()
+								
+								var success = b.accept_item_node(new_item, null)
+								if success:
+									items_buffered.remove_at(0)
+									inventory_changed.emit()
 									
-				elif b.has_method("accept_item_node") and b.has_method("accepts_item_at"):
-					if b.accepts_item_at(adj_pos):
-						var item_scene = load("res://scenes/buildings & related/belts & items/item.tscn")
-						var new_item = item_scene.instantiate()
-						if new_item.has_method("setup"): new_item.setup(level_ref)
-						new_item.item_data = next_item
-						
-						var success = b.accept_item_node(new_item, null)
-						if success:
-							items_buffered.remove_at(0)
-							inventory_changed.emit()
-							
-							if is_selected:
-								var hud = get_tree().get_first_node_in_group("MainHUD")
-								if hud and hud.has_node("Popup_Layer/DetailMenu"):
-									hud.get_node("Popup_Layer/DetailMenu").refresh_ui()
-						else:
-							new_item.queue_free()
+									if is_selected:
+										var hud = get_tree().get_first_node_in_group("MainHUD")
+										if hud and hud.has_node("Popup_Layer/DetailMenu"):
+											hud.get_node("Popup_Layer/DetailMenu").refresh_ui()
+								else:
+									new_item.queue_free()
 
 
 
